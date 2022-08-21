d6bfc4f4684f25824b5658ef86b2dc2e4d97019a
Finnald <copsnrobbers95@gmail.com>
2022-08-21 10:10:24 +0800

    added order viewing

--- prototype.py
+++ prototype.py
@@ -6,8 +6,15 @@ import sqlite3
 
 app = Flask("__FoodOrderSystem__", template_folder="templates", static_folder="static")
 app.secret_key = "Dgjoewheighe"
-conn = sqlite3.connect("foodsystem.db")
 db = "foodsystem.db"
+conn = sqlite3.connect(db)
+
+def fetchQuery(query):
+    sqlite3.connect(db).row_factory = sqlite3.Row
+    cur=sqlite3.connect(db).cursor()
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
 
 
 # route for index.hmtl, will immediately redirect to login page
@@ -31,7 +38,7 @@ def login():
 
             with sqlite3.connect(db) as con:
                 cur = con.cursor()
-                cur.execute("INSERT INTO Users (UserName,FirstName,LastName,Password,UserType) VALUES(?,?,?,?)", (newUser,fname,lname,newPwd,"retail"))
+                cur.execute("INSERT INTO Users (UserName,FirstName,LastName,Password,UserType) VALUES(?,?,?,?,?)", (newUser,fname,lname,newPwd,"retail"))
                 con.commit()
                 print("succ")
                 msg = "Account created successfully"
@@ -43,7 +50,7 @@ def login():
             return render_template("login.html", msg = msg)
 
     if request.method =="GET":
-        session.pop("login", None)
+        session.clear()
         return render_template("login.html")
 
 @app.route("/createaccount", methods=["GET","POST"])
@@ -59,37 +66,54 @@ def validate():
         username = login["username"]
         pwd = login["pwd"]
         userlogin = (username, pwd)
+        type=0
         try:
             with sqlite3.connect(db) as con:
                 cur=con.cursor()
                 cur.execute("SELECT UserName, Password FROM Users")
                 userlist = cur.fetchall()
                 if userlogin in userlist:
-                    print("joey")
+                    print("user in login list")
                     cur.execute(f"SELECT UserType FROM Users WHERE UserName = '{username}'")
                     type = cur.fetchall()
                     confirm=True
                         
                 else:
-                    print("noey")
+                    print("user not in list")
                     confirm=False
         except:
-            print("bad")
+            print("process aborted")
             confirm = False
         finally:
             username = login["username"]
-            print(confirm)
             confirm=confirm
     elif request.method == "GET":
         confirm = False
     print(confirm)
-    return render_template("loginvalidation.html", username=username, confirm=confirm, type=type, retail=([('retail',)]))
+    return render_template("loginvalidation.html", username=username, confirm=confirm, type=type, retail=([('retail',)]), staff=([('staff',)]))
 
 
 @app.route("/retail/options", methods=["GET","POST"])
 def options():
     username = session["login"]["username"]
-    return render_template("retail.html", username = username)
+    orderlist=[]
+    try:
+        with sqlite3.connect(db) as con:
+            userID = fetchQuery(f"SELECT UserID FROM Users WHERE Username = '{username}'")
+            print(userID)
+            orderlist = fetchQuery(f"SELECT * FROM Orders WHERE UserID = '{userID[0][0]}'")
+            print(orderlist)
+    except:
+        print("exception")
+    return render_template("retail.html", username = username, orderlist=orderlist, count=0)
+
+@app.route("/orders/<OrderID>")
+def vieworder(OrderID):
+    try:
+        orderitems = fetchQuery(f"SELECT OrderItems.ItemID, Items.ItemName, ItemQuantity FROM OrderItems INNER JOIN Items on items.ItemID = OrderItems.ItemID WHERE OrderID = {OrderID}")
+    except:
+        print("exception")
+    return f"Page for order {OrderID}, {orderitems}"
 
 @app.route("/retail/createorder")
 def createorder():
